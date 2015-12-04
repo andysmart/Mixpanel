e52c4a868fe1d8d72a8ef407744eb0e692049dec
Andy Smart <andy@andysmart.org>
2015-12-04 21:25:43 +0000

    Chop out watchOS and MacOS, we only need this for tvOS

--- Mixpanel/Mixpanel.swift
+++ Mixpanel/Mixpanel.swift
@@ -7,14 +7,7 @@
 //
 
 import Foundation
-
-#if os(iOS)
-	import UIKit
-#elseif os(watchOS)
-	import WatchKit
-#elseif os(OSX)
-	import AppKit
-#endif
+import UIKit
 
 /// Simple wrapper for Mixpanel. All requests are sent to the network in the background. If there is no Internet
 /// connection, it will silently fail.
@@ -62,38 +55,15 @@ public struct Mixpanel {
 			properties["$model"] = deviceModel
 		}
 
-		#if os(iOS)
-			properties["mp_lib"] = "iphone"
-
-			let device = UIDevice.currentDevice()
-			properties["$os"] = device.systemName
-			properties["$os_version"] = device.systemVersion
-
-			let size = UIScreen.mainScreen().bounds.size
-			properties["$screen_width"] = UInt(size.width)
-			properties["$screen_height"] = UInt(size.height)
-
-		#elseif os(watchOS)
-			properties["mp_lib"] = "applewatch"
-
-			let device = WKInterfaceDevice.currentDevice()
-			properties["$os"] = device.systemName
-			properties["$os_version"] = device.systemVersion
-
-			properties["$screen_width"] = UInt(device.screenBounds.size.width)
-			properties["$screen_height"] = UInt(device.screenBounds.size.height)
-		#elseif os(OSX)
-			properties["mp_lib"] = "mac"
-
-			let processInfo = NSProcessInfo()
-			properties["$os"] = "Mac OS X"
-			properties["$os_version"] = processInfo.operatingSystemVersionString
-
-			if let size = NSScreen.mainScreen()?.frame.size {
-				properties["$screen_width"] = UInt(size.width)
-				properties["$screen_height"] = UInt(size.height)
-			}
-		#endif
+        properties["mp_lib"] = "iphone"
+        
+        let device = UIDevice.currentDevice()
+        properties["$os"] = device.systemName
+        properties["$os_version"] = device.systemVersion
+        
+        let size = UIScreen.mainScreen().bounds.size
+        properties["$screen_width"] = UInt(size.width)
+        properties["$screen_height"] = UInt(size.height)
 
 		return properties
 	}
